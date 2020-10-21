544aa1f17b034db2c02e0deecff308845ce5d7e5
kovryakoff23 <kovryakoff23@yandex.ru>
2020-10-21 17:07:55 +0300

    two commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import org.flywaydb.core.Flyway;
 
 public final class Main {
   public static void main(String[] args) {
-    final Flyway flyway=Flyway.configure().dataSource("jdbc:postgresql://127.0.0.1:5432/Test","postgres","2357")
+    final Flyway flyway=Flyway.configure().dataSource("jdbc:postgresql://127.0.0.1:5432/Test1","postgres","2357")
         .locations("db")
         .load();
     flyway.clean();
--- src/main/resources/db/V1__create.sql
+++ src/main/resources/db/V1__create.sql
@@ -1,50 +1,74 @@
+CREATE SEQUENCE public.Courses_id
+    INCREMENT 1
+    START 0
+    MINVALUE 0
+    MAXVALUE 9999;
+
 CREATE TABLE Courses (
     Courses_id bigint NOT NULL,
     Name char[],
-    Number_hours integer
-    CONSTRAINT Courses_primary_key PRIMARY KEY (Courses_id);
+    Number_hours integer,
+    CONSTRAINT Courses_primary_key PRIMARY KEY (Courses_id)
+        INCLUDE(Courses_id)
 );
+
+CREATE SEQUENCE public.Teacher_id
+    INCREMENT 1
+    START 0
+    MINVALUE 0
+    MAXVALUE 9999;
 
 CREATE TABLE Teacher (
     Teacher_id bigint NOT NULL,
     Name char[],
     Surname char,
-    Age integer char[],
-    Course_id integer
-    CONSTRAINT Teacher_primary_key PRIMARY KEY (Teacher_id);
-    CONSTRAINT Courses_id FOREIGN KEY (Course_id)
+    Age integer ,
+    Course_id integer,
+    CONSTRAINT Teacher_primary_key PRIMARY KEY (Teacher_id)
+        INCLUDE(Teacher_id),
+    CONSTRAINT Courses_id FOREIGN KEY (Courses_id)
         REFERENCES public.Courses (Courses_id)  MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
         NOT VALID
 );
 
+CREATE SEQUENCE public.Students_id
+    INCREMENT 1
+    START 0
+    MINVALUE 0
+    MAXVALUE 9999;
+
 CREATE TABLE public.Students (
     Student_id bigint NOT NULL,
     Name char[],
     Surname char[],
-    Taken boolean
-    CONSTRAINT Student_primary_key PRIMARY KEY (Student_id);
+    Taken boolean,
+    CONSTRAINT Student_primary_key PRIMARY KEY (Student_id)
+        INCLUDE(Students_id),
 );
 
+
+CREATE SEQUENCE public.Taken_id
+    INCREMENT 1
+    START 0
+    MINVALUE 0
+    MAXVALUE 9999;
+
 CREATE TABLE public.Student_taken (
+    Taken_id bigint NOT NULL,
     Student_id bigint NOT NULL,
     Course_id bigint NOT NULL,
-    Taken_id bigint NOT NULL
-    CONSTRAINT Taken_primary_key PRIMARY KEY (Taken_id);
+    CONSTRAINT Taken_primary_key PRIMARY KEY (Taken_id)
+            INCLUDE(Taken_id)
 );
 
 CREATE TABLE public.Marks (
-    Course_id bigint NOT NULL,
     Date date,
     Taken_id bigint NOT NULL,
     Mark_homework integer,
-    Attending boolean
-    CONSTRAINT Marks_pkey PRIMARY KEY (Course_id, Taken_id)
-    CONSTRAINT Course_id FOREIGN KEY (Course_id) REFERENCES public.Courses(Courses_id)  MATCH SIMPLE
-        ON UPDATE NO ACTION
-        ON DELETE NO ACTION
-        NOT VALID
+    Attending boolean,
+    CONSTRAINT Marks_pkey PRIMARY KEY (Taken_id),
     CONSTRAINT Taken_id FOREIGN KEY (Taken_id) REFERENCES public.Student_taken(Taken_id)   MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
@@ -54,11 +78,12 @@ CREATE TABLE public.Marks (
 CREATE TABLE public.Schedule (
     Course_id bigint,
     Teacher_id bigint,
-    Data date
+    Data date,
+    CONSTRAINT Schedule_primary_key PRIMARY KEY (Data, Course_id),
     CONSTRAINT Course_id FOREIGN KEY (Course_id) REFERENCES public.Courses(Courses_id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
-        NOT VALID
+        NOT VALID,
     CONSTRAINT Teacher_id FOREIGN KEY (Teacher_id) REFERENCES public.Teacher(Teacher_id) MATCH SIMPLE
         ON UPDATE NO ACTION
         ON DELETE NO ACTION
